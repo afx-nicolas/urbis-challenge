91476b6c83298c624a04d30e91aaeac7460f7045
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-15 20:00:09 -0300

    feat: Add useCredentials custom hook

---
+++ hooks/useCredentials.ts
@@ -0,0 +1,40 @@
+import { useReducer } from 'react';
+
+interface Credentials {
+  email: string;
+  password: string;
+}
+
+const initialValue: Credentials = {
+  email: '',
+  password: '',
+};
+
+interface CredentialsReducerActions {
+  type: 'updateEmail' | 'updatePassword' | 'clearEmail' | 'clearPassword';
+  value?: string;
+}
+
+function credentialsReducer(
+  state: Credentials,
+  action: CredentialsReducerActions
+) {
+  switch (action.type) {
+    case 'updateEmail':
+      return { ...state, email: action.value || '' };
+    case 'updatePassword':
+      return { ...state, password: action.value || '' };
+    case 'clearEmail':
+      return { ...state, email: '' };
+    case 'clearPassword':
+      return { ...state, password: '' };
+    default:
+      throw new Error(`Unknown action ${action.type}`);
+  }
+}
+
+export default function useCredentials() {
+  const [credentials, dispatch] = useReducer(credentialsReducer, initialValue);
+
+  return { credentials, dispatch };
+}
