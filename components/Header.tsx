308f19dc08c162c98dac4ae7d6a9de0ea07597d2
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-15 11:22:51 -0300

    feat: Add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import styles from '../styles/Beneficios.module.css';
+import {
+  AccountCircle,
+  NotificationImportant,
+  Search,
+} from '../components/Icons';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <span className={styles.logo}>Logo</span>
+        <div className={styles.searchContainer}>
+          <div className={styles.searchBarWrapper}>
+            <input
+              className={styles.searchBar}
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Buscar"
+            />
+            <div className={styles.searchIconWrapper}>
+              <Search />
+            </div>
+          </div>
+        </div>
+        <div className={styles.profile}>
+          <span>Olá, Fulano!</span>
+          <div className={styles.profileButtonsWrapper}>
+            <button className={styles.profileButton}>
+              <NotificationImportant />
+            </button>
+            <a href="#" className={styles.profileButton}>
+              <AccountCircle />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
