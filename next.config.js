8fd186510abae152e2b448c563dec9c0a2eabd26
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-15 11:16:53 -0300

    chore: Add urbis image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['urbis-data-drive-api.s3.amazonaws.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
