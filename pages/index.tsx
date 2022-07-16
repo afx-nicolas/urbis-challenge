115bc3b190fa3d6b5171e16fbb4ea711301b29e9
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-16 12:22:14 -0300

    feat: Check user session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import type { NextPage } from 'next';
-import React, { useState } from 'react';
+import type { NextPage, NextPageContext } from 'next';
+import { parseCookies } from 'nookies';
+import { useState } from 'react';
 
 import useCredentials from '../hooks/useCredentials';
+import { auth } from '../lib/auth';
 
 import styles from '../styles/Home.module.css';
+
+const emailRegex = /^[A-z0-9.\-_]+@[A-z0-9.\-_]+\.[A-z]{2,4}(\.[A-z]{2,4})?$/;
 
 const Home: NextPage = () => {
   const [isEmailValid, setIsEmailValid] = useState(true);
@@ -16,17 +20,23 @@ const Home: NextPage = () => {
   }
 
   function handlePasswordInput(e: React.ChangeEvent<HTMLInputElement>) {
-    dispatch({ type: 'updateEmail', value: e.target.value });
-    setIsValidCredentials(true);
+    dispatch({ type: 'updatePassword', value: e.target.value });
   }
 
-  function handleSubmit(e: React.FormEvent) {
+  async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    const { email } = credentials;
+    const { email, password } = credentials;
 
-    setIsEmailValid(
-      !!/^[A-z0-9.\-_]+@[A-z0-9.\-_]+\.[A-z]{2,4}(\.[A-z]{2,4})?$/.exec(email)
-    );
+    if (!emailRegex.exec(email)) {
+      return setIsEmailValid(false);
+    }
+
+    const isAuthenticated = await auth(credentials);
+
+    if (!isAuthenticated) {
+      dispatch({ type: 'clearPassword' });
+      return setIsValidCredentials(false);
+    }
   }
 
   return (
@@ -80,3 +90,20 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  const { token } = parseCookies(ctx, 'token');
+
+  if (token) {
+    return {
+      redirect: {
+        destination: '/beneficios',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
