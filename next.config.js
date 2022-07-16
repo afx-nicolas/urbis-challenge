2ab59dfd665844b9bc58238b0cc703f446f5b314
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-16 00:37:53 -0300

    Add firebase image domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['urbis-data-drive-api.s3.amazonaws.com'],
+    domains: [
+      'urbis-data-drive-api.s3.amazonaws.com',
+      'firebasestorage.googleapis.com',
+    ],
   },
 };
 
