d556bf9e49982f216300b2008059df0dc56f2aa3
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-16 00:37:16 -0300

    Add Benefit type

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,14 @@ export interface Credentials {
   email: string;
   password: string;
 }
+
+export interface Benefit {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  discount: string;
+  rules: string;
+  isOnline: boolean;
+  url: string | null;
+}
