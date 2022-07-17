32ab12f5d88b4fded62a6c057b21af5cc2a83bcb
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 19:53:10 -0300

    feat: Dynamic notifications display

--- src/components/Notifications/index.tsx
+++ src/components/Notifications/index.tsx
@@ -1,40 +1,45 @@
 import styles from './Notifications.module.css';
 
-export default function Notifications() {
+interface NotificationsProps {
+  notifications: string[];
+  handleModalOpen: () => void;
+  setNotificationForId: (id: string) => void;
+  closeNotifications: () => void;
+}
+
+export default function Notifications({
+  notifications,
+  handleModalOpen,
+  setNotificationForId,
+  closeNotifications,
+}: NotificationsProps) {
+  function modalOpenForId(id: string) {
+    closeNotifications();
+    handleModalOpen();
+    setNotificationForId(id);
+  }
+
   return (
     <div className={styles.container}>
       <span className={styles.header}>Notificações</span>
       <div className={styles.notificationsWrapper}>
-        <div className={styles.notification}>
-          <span className={styles.notificationTitle}>
-            Você usou um benefício? conta para a gente!
-          </span>
-          <span className={styles.notificationBody}>
-            Você acessou o site de um parceiro, gostaríamos de saber se você
-            usou algum benefício.{' '}
-            <span className={styles.modalOpen}>responder questionário</span>
-          </span>
-        </div>
-        <div className={styles.notification}>
-          <span className={styles.notificationTitle}>
-            Você usou um benefício? conta para a gente!
-          </span>
-          <span className={styles.notificationBody}>
-            Você acessou o site de um parceiro, gostaríamos de saber se você
-            usou algum benefício.{' '}
-            <span className={styles.modalOpen}>responder questionário</span>
-          </span>
-        </div>
-        <div className={styles.notification}>
-          <span className={styles.notificationTitle}>
-            Você usou um benefício? conta para a gente!
-          </span>
-          <span className={styles.notificationBody}>
-            Você acessou o site de um parceiro, gostaríamos de saber se você
-            usou algum benefício.{' '}
-            <span className={styles.modalOpen}>responder questionário</span>
-          </span>
-        </div>
+        {notifications.map((notification) => (
+          <div key={notification} className={styles.notification}>
+            <span className={styles.notificationTitle}>
+              Você usou um benefício? conta para a gente!
+            </span>
+            <span className={styles.notificationBody}>
+              Você acessou o site de um parceiro, gostaríamos de saber se você
+              usou algum benefício.{' '}
+              <button
+                onClick={() => modalOpenForId(notification)}
+                className={styles.modalOpen}
+              >
+                responder questionário
+              </button>
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
