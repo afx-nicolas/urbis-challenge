2dc2ad8f4e194a0f2f57bce6bab7ba2ff4ea190f
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 16:29:57 -0300

    refactor: Use SearchBar component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,9 @@
 import { useContext } from 'react';
 
 import styles from './Header.module.css';
-import {
-  AccountCircle,
-  NotificationImportant,
-  Search,
-} from '../../components/Icons';
+import { AccountCircle, NotificationImportant } from '../../components/Icons';
 import { UserContext } from '../../contexts/UserContext';
+import SearchBar from '../SearchBar';
 
 export default function Header() {
   const { name } = useContext(UserContext);
@@ -15,20 +12,7 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <span className={styles.logo}>Logo</span>
-        <div className={styles.searchContainer}>
-          <div className={styles.searchBarWrapper}>
-            <input
-              className={styles.searchBar}
-              type="search"
-              name="search"
-              id="search"
-              placeholder="Buscar"
-            />
-            <div className={styles.searchIconWrapper}>
-              <Search />
-            </div>
-          </div>
-        </div>
+        <SearchBar />
         <div className={styles.profile}>
           <span>Olá {name}!</span>
           <div className={styles.profileButtonsWrapper}>
