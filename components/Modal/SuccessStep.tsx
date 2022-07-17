6b531c130a3d1c1b9eee2d6e527bd81a7f0d56d2
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-16 22:05:04 -0300

    feat: Add success modal step

---
+++ components/Modal/SuccessStep.tsx
@@ -0,0 +1,27 @@
+import styles from './Modal.module.css';
+
+import { Insignia } from '../Icons';
+import Button from '../Button';
+
+interface SuccessStepProps {
+  submitFeedback: () => void;
+}
+
+export default function SuccessStep({ submitFeedback }: SuccessStepProps) {
+  return (
+    <>
+      <Insignia />
+      <span className={styles.title}>Sucesso!</span>
+      <p className={styles.description}>
+        Sua resposta foi registrada com sucesso. Agradecemos muito por você ter
+        dedicado esse tempo para nos ajudar a tornar nosso clube cada vez
+        melhor.
+      </p>
+      <div className={styles.buttonContainer}>
+        <Button onClick={submitFeedback} variant="primary">
+          Fechar
+        </Button>
+      </div>
+    </>
+  );
+}
