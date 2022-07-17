c8ff03858d4469f0110058defedeb1e5f4ad8683
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 15:11:48 -0300

    feat: Add PageHead component

---
+++ src/components/PageHead.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+interface PageHeadProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function PageHead({ title, children }: PageHeadProps) {
+  return (
+    <>
+      <Head>
+        <title>{title} | Clube Staging</title>
+        <meta
+          name="description"
+          content="Aproveite todos os benefícios do clube de vantagens Staging"
+        />
+      </Head>
+      {children}
+    </>
+  );
+}
