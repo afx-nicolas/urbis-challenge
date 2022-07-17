bb4b4a44287029b4bc84bff3bdb9ff83ddcc79c6
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 13:35:26 -0300

    chore: Move header component to Header folder

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
 
-import styles from '../styles/Beneficios.module.css';
+import styles from './Header.module.css';
 import {
   AccountCircle,
   NotificationImportant,
   Search,
-} from '../components/Icons';
-import { UserContext } from '../contexts/UserContext';
+} from '../../components/Icons';
+import { UserContext } from '../../contexts/UserContext';
 
 export default function Header() {
   const { name } = useContext(UserContext);
