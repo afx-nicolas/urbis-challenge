912381a551b59f99ba2c5de29baa8910c16ee967
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-16 23:16:59 -0300

    feat: Add Pagination component

---
+++ components/Pagination/index.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import styles from './Pagination.module.css';
+
+interface PaginationProps {
+  page: number;
+  maxPages: number;
+}
+
+export default function Pagination({ page, maxPages }: PaginationProps) {
+  const remainingPages = maxPages - page;
+  console.log(remainingPages);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        {[...Array(remainingPages > 4 ? 9 : remainingPages + 5)].map(
+          (_, index) => {
+            const initialPageLink = page - 5;
+            const offset = initialPageLink >= 0 ? initialPageLink : 0;
+
+            return (
+              <Link
+                passHref
+                key={index}
+                href={{
+                  pathname: '/beneficios',
+                  query: { page: index + offset + 1 },
+                }}
+              >
+                <a
+                  className={
+                    +page === index + offset + 1
+                      ? [styles.link, styles.currentPage].join(' ')
+                      : styles.link
+                  }
+                >
+                  {index + offset + 1}
+                </a>
+              </Link>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
+}
