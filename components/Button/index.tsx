2af46c53f4bac73264a108966745fa9c863ae4b4
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 14:08:00 -0300

    feat: Add disabled variant

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,21 +1,51 @@
 import styles from './Button.module.css';
 
 interface ButtonProps {
+  className?: string;
   onClick?: (event: React.MouseEvent) => void;
-  variant: 'primary' | 'secondary';
+  variant: 'primary' | 'secondary' | 'disabled';
+  isLink: boolean;
+  href?: string;
+  target?: string;
   disabled?: boolean;
   children: React.ReactNode;
 }
 
 export default function Button({
+  className,
   onClick,
   variant,
+  isLink = false,
+  href,
+  target,
   disabled,
   children,
 }: ButtonProps) {
   return (
-    <button onClick={onClick} disabled={disabled} className={styles[variant]}>
-      {children}
-    </button>
+    <>
+      {isLink && !disabled ? (
+        <a
+          onClick={onClick}
+          className={
+            className ? [styles[variant], className].join(' ') : styles[variant]
+          }
+          href={href}
+          target={target}
+          rel={target && 'noreferrer noopener'}
+        >
+          {children}
+        </a>
+      ) : (
+        <button
+          onClick={onClick}
+          disabled={disabled}
+          className={
+            className ? [styles[variant], className].join(' ') : styles[variant]
+          }
+        >
+          {children}
+        </button>
+      )}
+    </>
   );
 }
