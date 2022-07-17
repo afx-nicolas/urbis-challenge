478957d53d36bacd2a528cd83ac47909775b8d36
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 15:13:44 -0300

    feat: Use PageHead component on login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
+import { useState } from 'react';
 import type { NextPage, NextPageContext } from 'next';
 import { parseCookies } from 'nookies';
-import { useState } from 'react';
-import { Spinner } from '../components/Icons';
+
+import styles from '../styles/Home.module.css';
 
 import useCredentials from '../hooks/useCredentials';
 import { auth } from '../lib/auth';
-
-import styles from '../styles/Home.module.css';
+import PageHead from '../components/PageHead';
+import { Spinner } from '../components/Icons';
 
 const emailRegex = /^[A-z0-9.\-_]+@[A-z0-9.\-_]+\.[A-z]{2,4}(\.[A-z]{2,4})?$/;
 
@@ -47,52 +48,54 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Clube de vantagens Staging</h1>
-        <h2>Login</h2>
-        {isValidCredentials ? null : (
-          <div className={styles.invalidCredentials}>
-            <span>E-mail ou senha incorretos</span>
-          </div>
-        )}
-        <form className={styles.form} onSubmit={handleSubmit} noValidate>
-          <div className={styles.inputGroup}>
-            <label htmlFor="email">E-mail</label>
-            <input
-              className={
-                isEmailValid
-                  ? styles.input
-                  : [styles.input, styles.invalid].join(' ')
-              }
-              type="email"
-              name="email"
-              value={credentials.email}
-              onChange={handleEmailInput}
-            />
-            {isEmailValid || (
-              <small className={styles.alert}>Digite um e-mail válido</small>
-            )}
-          </div>
-          <div className={styles.inputGroup}>
-            <label htmlFor="password">Senha</label>
-            <input
-              className={styles.input}
-              type="password"
-              name="password"
-              value={credentials.password}
-              onChange={handlePasswordInput}
-            />
-          </div>
-          <button className={styles.submitButton} type="submit">
-            {isLoading && <Spinner color="#fff" size={14} />} Entrar
-          </button>
-        </form>
-      </main>
-      <footer className={styles.footer}>
-        <small>Made with {'<3'} by Nicolas A Feitoza</small>
-      </footer>
-    </div>
+    <PageHead title="Login">
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Clube de vantagens Staging</h1>
+          <h2>Login</h2>
+          {isValidCredentials ? null : (
+            <div className={styles.invalidCredentials}>
+              <span>E-mail ou senha incorretos</span>
+            </div>
+          )}
+          <form className={styles.form} onSubmit={handleSubmit} noValidate>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">E-mail</label>
+              <input
+                className={
+                  isEmailValid
+                    ? styles.input
+                    : [styles.input, styles.invalid].join(' ')
+                }
+                type="email"
+                name="email"
+                value={credentials.email}
+                onChange={handleEmailInput}
+              />
+              {isEmailValid || (
+                <small className={styles.alert}>Digite um e-mail válido</small>
+              )}
+            </div>
+            <div className={styles.inputGroup}>
+              <label htmlFor="password">Senha</label>
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                value={credentials.password}
+                onChange={handlePasswordInput}
+              />
+            </div>
+            <button className={styles.submitButton} type="submit">
+              {isLoading && <Spinner color="#fff" size={14} />} Entrar
+            </button>
+          </form>
+        </main>
+        <footer className={styles.footer}>
+          <small>Made with {'<3'} by Nicolas A Feitoza</small>
+        </footer>
+      </div>
+    </PageHead>
   );
 };
 
