4ed64e22eb53de2d59bef0b419c1099b87194450
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 15:12:20 -0300

    feat: Use PageHead component on beneficios page

--- src/pages/beneficios.tsx
+++ src/pages/beneficios.tsx
@@ -6,6 +6,7 @@ import { api } from '../services/api';
 import { Benefit } from '../types';
 import { UserProvider } from '../contexts/UserContext';
 
+import PageHead from '../components/PageHead';
 import Card from '../components/Card';
 import Header from '../components/Header';
 import styles from '../styles/Beneficios.module.css';
@@ -36,27 +37,29 @@ export default function Beneficios({
 
   return (
     <UserProvider>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.container}>
-          {benefits.map((benefit) => (
-            <Card
-              key={benefit.id}
-              id={benefit.id}
-              image={benefit.image}
-              title={benefit.title}
-              description={benefit.description}
-              discount={benefit.discount}
-              rules={benefit.rules}
-              isOnline={benefit.isOnline}
-              url={benefit.url}
-              handleModalOpen={handleModalOpen}
-            />
-          ))}
-        </div>
-      </main>
-      <Pagination page={page} maxPages={maxPages} />
-      {isModalOpen && <Modal closeModal={handleModalClose} />}
+      <PageHead title="Benefícios">
+        <Header />
+        <main className={styles.main}>
+          <div className={styles.container}>
+            {benefits.map((benefit) => (
+              <Card
+                key={benefit.id}
+                id={benefit.id}
+                image={benefit.image}
+                title={benefit.title}
+                description={benefit.description}
+                discount={benefit.discount}
+                rules={benefit.rules}
+                isOnline={benefit.isOnline}
+                url={benefit.url}
+                handleModalOpen={handleModalOpen}
+              />
+            ))}
+          </div>
+        </main>
+        <Pagination page={page} maxPages={maxPages} />
+        {isModalOpen && <Modal closeModal={handleModalClose} />}
+      </PageHead>
     </UserProvider>
   );
 }
