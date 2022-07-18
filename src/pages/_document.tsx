9cbf10085863f08ae75753fa6019501429a9a70a
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-07-17 21:10:53 -0300

    feat: Add Roboto font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
